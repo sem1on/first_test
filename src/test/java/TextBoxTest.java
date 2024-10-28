b8add74e81030ac6e30b8d6940405f63cd228e98
sem1on <sgorlov21@gmail.com>
2024-10-29 00:44:03 +0300

    add TextBoxTest

---
+++ src/test/java/TextBoxTest.java
@@ -0,0 +1,27 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void fillForText() {
+        open("https://demoqa.com/text-box");
+        $("[id=userName]").setValue("Jon Smith");
+        $("#userEmail").setValue("JSmith@gmail.ru");
+        $("#currentAddress").setValue("New York, Lenina str. 17");
+        $("[id=permanentAddress]").setValue("Chicago, Mira str. 23");
+        $("[id=submit]").click();
+        $("#output").shouldHave(text("Jon Smith"), text("JSmith@gmail.ru"),
+                text("New York, Lenina str. 17"), text("Chicago, Mira str. 23"));
+    }
+}
