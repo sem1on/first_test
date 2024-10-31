d8a7bec88776e7ec1117291fa68957356c437bd2
sem1on <sgorlov21@gmail.com>
2024-11-01 00:05:20 +0300

    add Drug And Drop

---
+++ src/test/java/DrugAndDrop.java
@@ -0,0 +1,17 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class DrugAndDrop {
+
+    @Test
+    void drugAndDrop() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadTimeout = 600000;
+        Configuration.holdBrowserOpen = true;
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDrop(DragAndDropOptions.to("#column-b"));
+    }
+}
